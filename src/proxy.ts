0b3618dba7f5a4f4789ca6c56679481e24562e73
MoYuan-CN <m56042764@163.com>
2026-03-22 19:11:59 +0800

    fix: using new api

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,11 +2,9 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import jwt from "jsonwebtoken";
 
-export const runtime = "nodejs";
-
 const JWT_SECRET = process.env.JWT_SECRET!;
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
     const { pathname } = req.nextUrl;
 
     // ✅ 放行 login 页面

990abfb2ec86badc737d6dce0ae826c33eb339df
MoYuan-CN <m56042764@163.com>
2026-08-05 20:30:24 +0800

    chore: update build script

--- build.sh
+++ build.sh
@@ -2,10 +2,14 @@
 
 # Stop Service
 systemctl stop website
+echo "Website Service Stopped"
 
 # Build Service
+echo "Building Website Service..."
 rm -rf .next
 pnpm build
+echo "Website Service Built"
 
 # Start Service
 systemctl start website
+echo "Website Service Started"

7e81f9db9a1dba7474f385318f85bacdb0f7196d
MoYuan-CN <m56042764@163.com>
2026-08-27 19:43:52 +0800

    feat: use dynamic background

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,15 @@ export default function RootLayout({
         <html lang="en" suppressHydrationWarning className={cn("antialiased", "font-sans", figtree.variable)}>
             <body>
                 <ThemeProvider>
+                    <video
+                        muted
+                        controls={false}
+                        autoPlay
+                        loop
+                        playsInline
+                        className="fixed inset-0 -z-10 h-full w-full object-cover">
+                        <source src="/images/background.webm" type="video/webm" />
+                    </video>
                     <div className="z-10 flex min-h-screen flex-col backdrop-blur-[2px]">{children}</div>
                     <Toaster />
                 </ThemeProvider>
